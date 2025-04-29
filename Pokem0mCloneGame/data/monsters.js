f4946a3ad5a48a4931b3b39040daf5310b2a3edc
napolitr <72556582+napolitr@users.noreply.github.com>
2025-04-30 01:10:59 +0300

    Add files via upload

---
+++ Pokem0mCloneGame/data/monsters.js
@@ -0,0 +1,35 @@
+const monsters = {
+  Emby: {
+    position: {
+      x: 250,
+      y: 150
+    },
+    image: {
+      src: './img/embySprite.png'
+    },
+    frames: {
+      max: 4,
+      hold: 30
+    },
+    animate: true,
+    name: 'Emby',
+    attacks: [attacks.Tackle, attacks.Fireball]
+  },
+  Draggle: {
+    position: {
+      x: 750,
+      y: -50
+    },
+    image: {
+      src: './img/draggleSprite.png'
+    },
+    frames: {
+      max: 4,
+      hold: 30
+    },
+    animate: true,
+    isEnemy: true,
+    name: 'Draggle',
+    attacks: [attacks.Tackle, attacks.Fireball]
+  }
+}
